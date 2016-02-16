5debccfc11b254e71b70a9a36c51f6905b740b61
chenqiulong <crap_man@163.com>
2016-02-16 14:36:10 +0800

    小改

---
+++ app/src/main/java/io/github/cellzer/yuezhihu/yuezhihu/ui/fragment/ChosenFragment.java
@@ -0,0 +1,17 @@
+package io.github.cellzer.yuezhihu.yuezhihu.ui.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by walmand_ on 2016/2/16 0016.
+ * 知乎精选一级页面
+ */
+public class ChosenFragment extends BaseFragment {
+    @Override
+    protected View initView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return null;
+    }
+}
--- app/src/main/java/io/github/cellzer/yuezhihu/yuezhihu/ui/fragment/NewsFragment.java
+++ app/src/main/java/io/github/cellzer/yuezhihu/yuezhihu/ui/fragment/NewsFragment.java
@@ -7,7 +7,6 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
